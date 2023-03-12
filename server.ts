3d4bdf2e67c203322b03adc66c277c3131d0ee01
Federico Miras <miras.federico@gmail.com>
2023-03-12 00:27:26 -0300

    feat: add corresponding args to chromium launch

--- server.ts
+++ server.ts
@@ -5,7 +5,10 @@ const app = express()
 const port = process.env.PORT || 3000
 
 async function main() {
-  const browser = await playwright.chromium.launch()
+  const browser = await playwright.chromium.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    ignoreDefaultArgs: ['--disable-extensions']
+  })
   const page = await browser.newPage()
   await page.goto('https://fmiras.com')
   await page.screenshot({ path: 'fmiras.png' })
